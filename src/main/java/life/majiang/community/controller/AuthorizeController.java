b23f51abd762588e9e9ce5723a8a0aa8eba85623
noviceinlife <15668049572@163.com>
2019-09-09 10:51:22 +0800

    add AuthorizeController

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
 
@@ -22,7 +25,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
-                           @RequestParam(name = "state")String state){
+                           @RequestParam(name = "state")String state,
+                            HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id("clientId");
         accessTokenDTO.setClient_secret("clientsecret");
@@ -31,7 +35,13 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.GetAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        if( user !=null){
+            request.getSession().setAttribute("user",user);
+            return "redirect:index";
+            //登陆成功，写cookie和session
+        }else{
+            //登录失败，重新登录
+            return "redirect:index";
+        }
     }
 }
